b3ee69e5cf2533e1a0c1f76c6b8d9ff2a5e0c3fc
mhunter02 <bighunterbg@gmail.com>
2022-09-21 13:37:14 +0100

    Debugging task

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello I am programmer");
-        System.out.print("I am studying in TUM - Campus Heilbronn");
-        System.out.print("I will fix this code so it will work correctly");
-        System.out.println("I will spot the defect with the process of debugging");
-        System.out.print("After that I will be ready with the Pre-course");
-        System.out.println("I am looking forward to start my studies in the next week!");
+        System.out.println("Time for debugging");
+        System.out.print("Today I learned about: ");
+        System.out.println("IDE");
+        System.out.println("Basic operations and variables");
+        System.out.print("Very simple control flow and functions");
+        System.out.println("Debugging");
     }
 }
 
