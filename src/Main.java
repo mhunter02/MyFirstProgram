b2348ac339cd3e7686f8d698133ea63847e28d14
mhunter02 <bighunterbg@gmail.com>
2022-09-21 13:01:48 +0100

    Debugging task

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello I am programmer");
+        System.out.print("I am studying in TUM - Campus Heilbronn");
+        System.out.print("I will fix this code so it will work correctly");
+        System.out.println("I will spot the defect with the process of debugging");
+        System.out.print("After that I will be ready with the Pre-course");
+        System.out.println("I am looking forward to start my studies in the next week!");
     }
 }
 
